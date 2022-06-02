d8cfdf37dd8af4b1ad36f3389b92d997a2eb9bad
Eduard Kyvenko <eduard.kyvenko@gmail.com>
2022-06-02 15:27:46 +0200

    Add an integration test for locked account

--- tests/integraion.rs
+++ tests/integraion.rs
@@ -25,4 +25,15 @@ mod integraion {
 
         Ok(())
     }
+
+    #[test]
+    fn locked() -> Result<(), Box<dyn std::error::Error>> {
+        let mut cmd = Command::cargo_bin("rust-transaction-engine")?;
+        cmd.arg("./examples/locked.csv");
+        let stdout = String::from_utf8(cmd.assert().success().get_output().stdout.clone())?;
+
+        insta::assert_snapshot!(stdout);
+
+        Ok(())
+    }
 }
